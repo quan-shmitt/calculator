4bc24a1a7cff85b52fc5544ed1d75ab2e2ab6735
yetAnotherEgirl <emilymosforth@gmail.com>
2022-10-12 08:45:13 +0100

    locked window size

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -34,8 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(212, 261);
+            this.ClientSize = new System.Drawing.Size(200, 250);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(216, 289);
+            this.MinimumSize = new System.Drawing.Size(216, 289);
             this.Name = "Form1";
             this.Text = "hello";
             this.Load += new System.EventHandler(this.Form1_Load);
